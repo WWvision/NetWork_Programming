6e3bc046313c721f4c68c510e230998830a46459
WWvision <rlatmdwn31@gmail.com>
2023-03-28 18:21:57 +0900

    pull

---
+++ name.py
@@ -0,0 +1,13 @@
+Name = "My name is Kimseungju"
+
+print(len(Name))
+print(Name * 10)
+print(Name[0])
+print(Name[:4])
+print(Name[-4:])
+print(Name[::-1])
+print(Name[1:len(Name)])
+
+print(Name.upper())
+print(Name.lower())
+print(Name.replace('a', 'b'))
